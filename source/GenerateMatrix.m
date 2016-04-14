f5ad197288bed5bde8316624c9f71f91c1b2387b
Adam R Claxton <adam.claxton@uconn.edu>
2016-04-14 01:19:17 -0400

    Reorginized project for compatibility
    Renamed pagerank function to GenerateMatrix, and created new function
    called PageRank which calls GenerateMatrix to build the adjacency matrix
    used in the rest of the algorithm.
    
    PageRank modifies the adjacency matrix to accommodate the damping
    factor, then iteratively recalculates a state vector using this
    transition matrix (right now the loop for that doesn't work). It appears
    that the most efficient way to calculate a new state vector is by matrix
    multiplication with the transition matrix and the existing state vector.
    
    We still need to determine when we have reached a steady state, and use
    the steady state vector to rank the original entries.

---
+++ source/GenerateMatrix.m
@@ -0,0 +1,18 @@
+function pmatrix = generateMatrix(nodes, sourcedest)
+%This function will run the page rank algorithm on an input of page
+%information.
+
+%make nxn matrix n = nodes
+pmatrix = zeros(nodes, nodes);
+fprintf('Solving pmatrix now.\n');
+for i = 1:length(sourcedest)
+   %count number of times j shows up in the source column
+   sourcenode = sourcedest(i,1);
+   destnode = sourcedest(i,2);
+   outgoinglinks = sum(sourcedest(:,1) == sourcenode);
+   %put into matrix
+   pmatrix(destnode,sourcenode) = 1/outgoinglinks;
+end
+
+end
+
