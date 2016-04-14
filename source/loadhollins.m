5d4f830c052b26b71e56fff818303cb837d6c399
clj13001 <connor.jackson@uconn.edu>
2016-04-13 21:22:49 -0400

    Hollins load code added as function.

---
+++ source/loadhollins.m
@@ -0,0 +1,36 @@
+function [ Nodes, Edges, IndexLinks, SourceDest ] = loadhollins( inputfile )
+%This function loads the input file and organizes it into easier to view
+%variables.
+
+%Open the file
+fileID = fopen(inputfile);
+%Translate into sloppy cell array; this makes a string cell array so we
+%need to convert them to numbers later on.
+cellarrayfile = textscan(fileID,'%s%s');
+%reorganize so that it's actually reasonable to obtain data that we need.
+col1 = cellarrayfile{1};
+col2 = cellarrayfile{2};
+cellfile = [col1 col2];
+
+%Get nodes and edges
+Nodes = str2num(cellfile{1,1});
+Edges = str2num(cellfile{1,2});
+
+%Form our data matrices separately
+IndexLinks = cellfile(2:Nodes+1,:);
+SourceDest = cellfile(Nodes+2:end,:);
+
+%For ease we'll convert all the numbers from string to actual number
+fprintf('Converting indices and links.\n');
+for i = 1:length(IndexLinks)
+    IndexLinks{i,1} = str2num(IndexLinks{i,1});
+end
+fprintf('Converting source and destination nodes.\n');
+for i = 1:length(SourceDest)
+    SourceDest{i,1} = str2num(SourceDest{i,1});
+    SourceDest{i,2} = str2num(SourceDest{i,2});
+end
+
+SourceDest = cell2mat(SourceDest);
+end
+
--- source/main.m
+++ source/main.m
@@ -7,38 +7,6 @@
 clc;
 clear;
 
-%Open the file
-fileID = fopen('hollins.dat');
-%Translate into sloppy cell array; this makes a string cell array so we
-%need to convert them to numbers later on.
-cellarrayfile = textscan(fileID,'%s%s');
-%reorganize so that it's actually reasonable to obtain data that we need.
-col1 = cellarrayfile{1};
-col2 = cellarrayfile{2};
-cellfile = [col1 col2];
-
-%Get nodes and edges
-Nodes = str2num(cellfile{1,1});
-Edges = str2num(cellfile{1,2});
-
-%Form our data matrices separately
-IndexLinks = cellfile(2:Nodes+1,:);
-SourceDest = cellfile(Nodes+2:end,:);
-
-%For ease we'll convert all the numbers from string to actual number
-fprintf('Converting indices and links.\n');
-for i = 1:length(IndexLinks)
-    IndexLinks{i,1} = str2num(IndexLinks{i,1});
-end
-fprintf('Converting source and destination nodes.\n');
-for i = 1:length(SourceDest)
-    SourceDest{i,1} = str2num(SourceDest{i,1});
-    SourceDest{i,2} = str2num(SourceDest{i,2});
-end
-
-SourceDest = cell2mat(SourceDest);
-
-%%Just to display variables we care about up to this point.
-clearvars cellarrayfile cellfile col1 col2 fileID i
+[Nodes, Edges, IndexLinks, SourceDest] = loadhollins('hollins.dat');
 
 
