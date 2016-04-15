4a6c488a107061e9f83dab70a022787f28307c4a
clj13001 <connor.jackson@uconn.edu>
2016-04-14 22:29:11 -0400

    Added in extra damping factor page lists.

--- source/main.m
+++ source/main.m
@@ -9,5 +9,7 @@ clear;
 
 %%Load the hollins file into these variables.
 [nodes, edges, indexlinks, sourcedest] = loadhollins('hollins.dat');
-rankedpages = pagerank(nodes, indexlinks, sourcedest, 0.85);
+rankedpages1 = pagerank(nodes, indexlinks, sourcedest, 0.85);
+rankedpages2 = pagerank(nodes, indexlinks, sourcedest, 0.95);
+rankedpages3 = pagerank(nodes, indexlinks, sourcedest, 0.50);
 
